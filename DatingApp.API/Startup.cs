d03687c24b8a914a306900cd9d8d5e4607251419
mr.furkankaracan@gmail.com <mr.furkankaracan@gmail.com>
2020-02-07 23:27:06 +0300

    Action Filter Handling

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,6 +36,7 @@ namespace DatingApp.API
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
             services.AddCors();
+            services.AddScoped<LogUserActivity>();
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
